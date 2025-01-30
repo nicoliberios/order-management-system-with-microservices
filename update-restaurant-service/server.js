aa16997a29274ccd32c2894c3efbab15ae2a4e5b
camilarleth25 <arletherazo25@hotmail.com>
2025-01-29 23:53:19 -0500

    feat: :art: Add two changes to the server file

--- update-restaurant-service/server.js
+++ update-restaurant-service/server.js
@@ -12,10 +12,12 @@ app.get('/', (req, res) => {
   res.status(200).json({ status: 'ok', message: 'update-restaurant-service Running' });
 });
 
+// This allows security between the front and the back
 app.use(cors({
   origin: 'load balancer'
 }));
 
+// These are the URLs
 app.use(bodyParser.json());
 app.use('/api', restaurantRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
