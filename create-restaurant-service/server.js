9731bda4cfb8fbeac092a8ed96ac98f883f4670b
nicoliberios <ingedesistemas24@gmail.com>
2025-02-05 00:30:47 -0500

    feat: :fire: This is a refactor

--- create-restaurant-service/server.js
+++ create-restaurant-service/server.js
@@ -5,6 +5,7 @@ const swaggerSpec = require('./swaggerConfig');
 const restaurantRoutes = require('./routes/restaurantRoutes');
 const cors = require('cors');
 
+
 const app = express();
 const port = 4013;
 
