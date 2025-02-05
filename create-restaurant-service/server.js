05fc376c352cef75011fc2731815920683cd1074
nicoliberios <ingedesistemas24@gmail.com>
2025-02-05 14:12:23 -0500

    feat: :art: This is a refactor file .yml

--- create-restaurant-service/server.js
+++ create-restaurant-service/server.js
@@ -18,6 +18,7 @@ app.use(cors({
   origin: 'http://lb-order-foot-.us-east-24578965551.elb.amazonaws.com'
 }));
 
+
 app.use(bodyParser.json());
 app.use('/api', restaurantRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
