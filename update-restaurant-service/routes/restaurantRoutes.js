135e98ab726976040833033b97d988946f233555
nicoliberios <ingedesistemas24@gmail.com>
2025-02-05 19:04:34 -0500

    feat: :art: This is a refactor file

--- update-restaurant-service/routes/restaurantRoutes.js
+++ update-restaurant-service/routes/restaurantRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const restaurantController = require('../controllers/restaurantController');
 
+
 /**
  * @swagger
  * components:
