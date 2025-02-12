c607e7421f507ed2c89f96247e99981bdfc3f608
nicoliberios <ingedesistemas24@gmail.com>
2025-02-12 16:39:55 -0500

    feat: :art: Add refactor code

--- create-dessert-service/server.js
+++ create-dessert-service/server.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./swaggerConfig');
-const restaurantRoutes = require('./routes/restaurantRoutes');  
+const swaggerSpec = require('./dessertsConfig');
+const dessertsRoutes = require('./routes/dessertsRoutes');
 const cors = require('cors');
 
 const app = express();
-const port = 4020;
+const port = 4014;
 
 app.get('/', (req, res) => {
   res.status(200).json({ status: 'ok', message: 'Microservice Running' });
 });
 
-
 app.use(cors({
-  origin: 'add url load balancer'
+  origin: ''
 }));
 
 app.use(bodyParser.json());
-app.use('/api', restaurantRoutes); 
+app.use('/api', dessertsRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
-  console.log(`Microservice read restaurant listening at http://localhost:${port}`);
-  console.log(`http://localhost:4020/api-docs`);
+  console.log(`Desserts service listening at http://localhost:${port}`);
+  console.log(`Desserts service listening at http://localhost:${port}/api/desserts`);
+  console.log(`Desserts service listening at http://localhost:${port}/api-docs`);
 });
