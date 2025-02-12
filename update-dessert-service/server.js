0564bd6f7f7bbcb919e36c404dec252bf998f6a1
nicoliberios <ingedesistemas24@gmail.com>
2025-02-12 18:47:44 -0500

    feat: :art: Add files for deploy

---
+++ update-dessert-service/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swaggerConfig');
+const dessertRoutes = require('./routes/dessertsRoutes');
+const cors = require('cors');
+
+const app = express();
+const port = 4011;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ status: 'ok', message: 'Microservice Running' });
+});
+
+app.use(cors({
+  origin: 'add url load balancer'
+}));
+
+app.use(bodyParser.json());
+app.use('/api', dessertRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.listen(port, () => {
+  console.log(`Dessert microservice is running at http://localhost:${port}`);
+});
