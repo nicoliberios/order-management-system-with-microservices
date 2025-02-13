ec74e7c60c7ec17c56afa9dde6c50b46a371fe1b
nicoliberios <ingedesistemas24@gmail.com>
2025-02-13 10:31:42 -0500

    feat:

--- user-microservice/models/db.js
+++ user-microservice/models/db.js
@@ -1,12 +1,18 @@
 const { Pool } = require('pg');
-const dbConfig = require('../config/dbConfig');  // Database configuration
 
-const pool = new Pool(dbConfig);
+const pool = new Pool({
+  host: 'dbpostgres.c5vvalnvdgee.us-east-1.rds.amazonaws.com', 
+  port: 5432,
+  user: process.env.DB_USER || 'postgres',  
+  password: process.env.DB_PASSWORD || 'holaholahola1.', 
+  database: process.env.DB_DATABASE || 'db-users'
+});
 
 pool.on('connect', () => {
   console.log('Connected to the PostgreSQL database');
 });
 
 module.exports = {
-  query: (text, params) => pool.query(text, params)  // Execute SQL queries
+  query: (text, params) => pool.query(text, params)
 };
+
